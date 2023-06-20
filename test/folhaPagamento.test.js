a781e840fd4416b945e801c62fb502a6d0aadc14
Gabriela <gabrielacamarchiori@gmail.com>
2023-06-20 11:07:22 -0300

    aula2 finalizada

---
+++ test/folhaPagamento.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line import/extensions
+import { calculaDescontos, somaHorasExtras } from '../index.js';
+
+describe('Testes dos cálculos de folhas', () => {
+  it('Deve retornar a soma das horas extras', () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras(2000, 500);
+
+    expect(retornado).toBe(esperado);
+  });
+
+  it('Deve descontar o valor do salário', () => {
+    const esperado = 2300;
+    const retornado = calculaDescontos(2500, 200);
+
+    expect(retornado).toBe(esperado);
+  });
+});
